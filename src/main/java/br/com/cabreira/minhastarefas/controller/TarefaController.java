a3731850003b258722059e63c5bf784e2ddc4213
leonardo.cabreira <leonardo.cabreira@objective.com.br>
2022-03-23 13:12:57 -0300

    [UN02-07] Criação de método de busca utilizando Spring Data repository findByDescricao

--- src/main/java/br/com/cabreira/minhastarefas/controller/TarefaController.java
+++ src/main/java/br/com/cabreira/minhastarefas/controller/TarefaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TarefaController {
@@ -14,8 +15,12 @@ public class TarefaController {
     private TarefaRepository repositorio;
 
     @GetMapping("/tarefa")
-    public List<Tarefa> todasTarefas(){
-        return repositorio.findAll();
+    public List<Tarefa> todasTarefas(@RequestParam Map<String,String> parametros){
+        if(parametros.isEmpty())
+            return repositorio.findAll();
+
+        String descricao = parametros.get("descricao");
+        return repositorio.findByDescricaoLike("%"+descricao+"%");
     }
 
     @GetMapping("/tarefa/{id}")
@@ -32,4 +37,5 @@ public class TarefaController {
     public void excluirTarefa(@PathVariable Integer id){
         repositorio.deleteById(id);
     }
+
 }
--- src/main/java/br/com/cabreira/minhastarefas/repository/TarefaRepository.java
+++ src/main/java/br/com/cabreira/minhastarefas/repository/TarefaRepository.java
@@ -3,5 +3,9 @@ package br.com.cabreira.minhastarefas.repository;
 import br.com.cabreira.minhastarefas.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TarefaRepository extends JpaRepository<Tarefa,Integer> {
+
+    List<Tarefa> findByDescricaoLike(String descricao);
 }
