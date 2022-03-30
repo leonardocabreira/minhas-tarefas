7cac72677a4a08ad6aff4418cdc56ef7ef6acd24
leonardo.cabreira <leonardo.cabreira@objective.com.br>
2022-03-30 07:24:49 -0300

    [UN03-07]criação das classes DTO para separar a controller da camada de serviços. Utilização da depedencia ModelMapper

--- src/main/java/br/com/cabreira/minhastarefas/controller/TarefaController.java
+++ src/main/java/br/com/cabreira/minhastarefas/controller/TarefaController.java
@@ -1,38 +1,50 @@
 package br.com.cabreira.minhastarefas.controller;
 
+import br.com.cabreira.minhastarefas.controller.response.TarefaResponse;
 import br.com.cabreira.minhastarefas.model.Tarefa;
-import br.com.cabreira.minhastarefas.repository.TarefaRepository;
 import br.com.cabreira.minhastarefas.services.TarefaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class TarefaController {
+
+    @Autowired
+    private ModelMapper mapper;
 
    @Autowired
     private TarefaService service;
 
     @GetMapping("/tarefa")
-    public List<Tarefa> todasTarefas(@RequestParam Map<String,String> parametros){
-        if(parametros.isEmpty())
-            return service.getTodasTarefas();
+    public List<TarefaResponse> todasTarefas(@RequestParam Map<String,String> parametros){
+        List<Tarefa> tarefas = new ArrayList<>();
 
-        String descricao = parametros.get("descricao");
-        return service.getTarefasPorDescricao(descricao);
+        if(parametros.isEmpty()) {
+            tarefas = service.getTodasTarefas();
+        }else {
+            String descricao = parametros.get("descricao");
+            tarefas = service.getTarefasPorDescricao(descricao);
+        }
+        final List<TarefaResponse> tarefasResponse = tarefas.stream().map(tarefa -> mapper.map(tarefa, TarefaResponse.class)).collect(Collectors.toList());
+        return tarefasResponse;
     }
 
     @GetMapping("/tarefa/{id}")
-    public Tarefa tarefa(@PathVariable Integer id){
-        return service.getTarefaPorId(id);
+    public TarefaResponse tarefa(@PathVariable Integer id){
+        final Tarefa tarefa = service.getTarefaPorId(id);
+        return mapper.map(tarefa, TarefaResponse.class);
     }
             /*produces = JSON*/
     @PostMapping(value = "/tarefa")
-    public Tarefa salvarTarefa(@Valid @RequestBody Tarefa tarefa){
-        return service.salvarTarefa(tarefa);
+    public TarefaResponse salvarTarefa(@Valid @RequestBody Tarefa tarefa){
+        return mapper.map(service.salvarTarefa(tarefa),TarefaResponse.class);
     }
 
     @DeleteMapping("/tarefa/{id}")
